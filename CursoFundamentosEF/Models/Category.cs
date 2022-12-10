be1725a2ffa880ca7189ac1828d0b0ffa93100b9
Jesús Rubén Esparza Soto <rubenesparzasoto95@gmail.com>
2022-12-10 10:50:17 -0700

    feat: add models

---
+++ CursoFundamentosEF/Models/Category.cs
@@ -0,0 +1,12 @@
+namespace CursoFundamentosEF.Models;
+
+public class Category
+{
+    public Guid CategoryId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public virtual ICollection<Task> Tasks { get; set; }
+}
---
+++ CursoFundamentosEF/Models/Task.cs
@@ -0,0 +1,25 @@
+namespace CursoFundamentosEF.Models;
+
+public class Task
+{
+    public Guid TaskId { get; set; }
+
+    public Guid CategoryId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public Priority PriorityTask { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual Category Category { get; set; }
+}
+
+public enum Priority
+{
+    Low = 1,
+    Medium,
+    High
+}
