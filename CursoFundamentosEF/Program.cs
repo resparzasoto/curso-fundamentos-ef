79c8e583c05d00ce3c5316605bec16b29718d088
Jesús Rubén Esparza Soto <40255084+resparzasoto@users.noreply.github.com>
2022-12-12 01:17:36 -0700

    feat: add database in memory

--- CursoFundamentosEF/Program.cs
+++ CursoFundamentosEF/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using CursoFundamentosEF;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/health", ([FromServices] TasksContext dbContext) => {
+    if (!dbContext.Database.EnsureCreated())
+        return Results.Ok("Database is not ready");
+
+    return Results.Ok($"Database in memory: { dbContext.Database.IsInMemory() }");
+});
 
 app.Run();
