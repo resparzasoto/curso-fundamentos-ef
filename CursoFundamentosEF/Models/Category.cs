6aa5bc613df026225e6acaf8cee4de88771f7c99
Jesús Rubén Esparza Soto <40255084+resparzasoto@users.noreply.github.com>
2022-12-12 10:36:22 -0700

    feat: change category model to fluent api

--- CursoFundamentosEF/Models/Category.cs
+++ CursoFundamentosEF/Models/Category.cs
@@ -1,14 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace CursoFundamentosEF.Models;
 
 public class Category
 {
-    [Key]
     public Guid CategoryId { get; set; }
 
-    [Required]
-    [MaxLength(150)]
     public string Name { get; set; }
 
     public string Description { get; set; }
--- CursoFundamentosEF/TasksContext.cs
+++ CursoFundamentosEF/TasksContext.cs
@@ -10,4 +10,15 @@ public class TasksContext : DbContext
     public DbSet<Models.Task> Tasks { get; set; }
 
     public TasksContext(DbContextOptions<TasksContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Models.Category>(category => {
+            category.ToTable("Category");
+            category.HasKey(t => t.CategoryId);
+
+            category.Property(t => t.Name).IsRequired().HasMaxLength(150);
+            category.Property(t => t.Description);
+        });
+    }
 }
