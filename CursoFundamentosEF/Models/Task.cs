04bda244df33e0cbc872cfc67768b8853f4ce0a0
Jesús Rubén Esparza Soto <40255084+resparzasoto@users.noreply.github.com>
2022-12-12 00:58:01 -0700

    feat: add data annotations.

--- CursoFundamentosEF/Models/Task.cs
+++ CursoFundamentosEF/Models/Task.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CursoFundamentosEF.Models;
 
 public class Task
 {
+    [Key]
     public Guid TaskId { get; set; }
 
+    [ForeignKey(nameof(CategoryId))]
     public Guid CategoryId { get; set; }
 
+    [Required]
+    [MaxLength(200)]
     public string Title { get; set; }
 
     public string Description { get; set; }
@@ -15,6 +22,9 @@ public class Task
     public DateTime CreatedAt { get; set; }
 
     public virtual Category Category { get; set; }
+
+    [NotMapped]
+    public string Summary { get; set; }
 }
 
 public enum Priority
