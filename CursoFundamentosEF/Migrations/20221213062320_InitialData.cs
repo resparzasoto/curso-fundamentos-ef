db9f87ffb3030133ea35f4a1df09716f0721d179
Jesús Rubén Esparza Soto <40255084+resparzasoto@users.noreply.github.com>
2022-12-13 12:53:22 -0700

    feat: add initial data migration

---
+++ CursoFundamentosEF/Migrations/20221213062320_InitialData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CursoFundamentosEF.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "CategoryId", "Description", "Name", "Weight" },
+                values: new object[,]
+                {
+                    { new Guid("45f3c893-8e07-4e4c-9f34-fd63f9552636"), "Category about learn activities", "Learn", 8 },
+                    { new Guid("d22d110c-1064-4a0a-a654-1f5a0f0856ea"), "Category about financial activities", "Financial", 10 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Task",
+                columns: new[] { "TaskId", "CategoryId", "CreatedAt", "Description", "PriorityTask", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("18c6b94f-5344-436b-b102-0301e98b1728"), new Guid("d22d110c-1064-4a0a-a654-1f5a0f0856ea"), new DateTime(2022, 12, 13, 6, 23, 20, 362, DateTimeKind.Utc).AddTicks(1589), null, 3, "Check bills of gifts" },
+                    { new Guid("90a13ed7-c79f-4a67-a081-bbef420fcf0b"), new Guid("45f3c893-8e07-4e4c-9f34-fd63f9552636"), new DateTime(2022, 12, 13, 6, 23, 20, 362, DateTimeKind.Utc).AddTicks(1593), null, 1, "Learn about compound interest" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "TaskId",
+                keyValue: new Guid("18c6b94f-5344-436b-b102-0301e98b1728"));
+
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "TaskId",
+                keyValue: new Guid("90a13ed7-c79f-4a67-a081-bbef420fcf0b"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("45f3c893-8e07-4e4c-9f34-fd63f9552636"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: new Guid("d22d110c-1064-4a0a-a654-1f5a0f0856ea"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Task",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- CursoFundamentosEF/TasksContext.cs
+++ CursoFundamentosEF/TasksContext.cs
@@ -13,6 +13,20 @@ public class TasksContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        List<Models.Category> categoriesInit = new List<Category>();
+        categoriesInit.Add(new Category() {
+            CategoryId = Guid.Parse("d22d110c-1064-4a0a-a654-1f5a0f0856ea"),
+            Name = "Financial",
+            Description = "Category about financial activities",
+            Weight = 10
+        });
+        categoriesInit.Add(new Category() {
+            CategoryId = Guid.Parse("45f3c893-8e07-4e4c-9f34-fd63f9552636"),
+            Name = "Learn",
+            Description = "Category about learn activities",
+            Weight = 8
+        });
+
         modelBuilder.Entity<Models.Category>(category => {
             category.ToTable("Category");
             category.HasKey(t => t.CategoryId);
@@ -20,6 +34,24 @@ public class TasksContext : DbContext
             category.Property(t => t.Name).IsRequired().HasMaxLength(150);
             category.Property(t => t.Description);
             category.Property(t => t.Weight);
+
+            category.HasData(categoriesInit);
+        });
+
+        List<Models.Task> tasksInit = new List<Models.Task>();
+        tasksInit.Add(new Models.Task() {
+            TaskId = Guid.Parse("18c6b94f-5344-436b-b102-0301e98b1728"),
+            CategoryId = Guid.Parse("d22d110c-1064-4a0a-a654-1f5a0f0856ea"),
+            Title = "Check bills of gifts",
+            PriorityTask = Priority.High,
+            CreatedAt = DateTime.UtcNow
+        });
+        tasksInit.Add(new Models.Task() {
+            TaskId = Guid.Parse("90a13ed7-c79f-4a67-a081-bbef420fcf0b"),
+            CategoryId = Guid.Parse("45f3c893-8e07-4e4c-9f34-fd63f9552636"),
+            Title = "Learn about compound interest",
+            PriorityTask = Priority.Low,
+            CreatedAt = DateTime.UtcNow
         });
 
         modelBuilder.Entity<Models.Task>(task => {
@@ -27,13 +59,15 @@ public class TasksContext : DbContext
             task.HasKey(t => t.TaskId);
 
             task.Property(t => t.Title).IsRequired().HasMaxLength(200);
-            task.Property(t => t.Description);
+            task.Property(t => t.Description).IsRequired(false);
             task.Property(t => t.PriorityTask).HasConversion(typeof(int));
             task.Property(t => t.CreatedAt).HasDefaultValueSql("now()");
 
             task.HasOne(t => t.Category).WithMany(t => t.Tasks).HasForeignKey(t => t.CategoryId);
 
             task.Ignore(t => t.Summary);
+
+            task.HasData(tasksInit);
         });
     }
 }
